494d75169afc571238a8576a6c9a733cc8669d3f
Valood <karataev090903@bk.ru>
2023-12-14 03:04:45 +0500

    Фильтрация товаров

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,13 +1,13 @@
 import { Navbar, Nav } from "react-bootstrap"
-import { NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export function CustomNavbar(){
     return (
         <Navbar className='navbar'>
             <Navbar.Brand>
-                <NavLink to='/' className='navbar-link navbar-logo'>Sepul'ka Shop</NavLink>
+                <Link to='/' className='navbar-link navbar-logo'>Sepul'ka Shop</Link>
             </Navbar.Brand>
-            <Nav className="ms-auto">
+            <Nav className="ms-auto d-flex gap-3">
                 <div>
                     <NavLink to='/login' className={({isActive}) => ['navbar-link', isActive ? 'active' : ''].join(' ')}>Вход</NavLink>
                 </div>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,7 @@
-import { Nav, Card, Button, Container, Row, Col } from 'react-bootstrap';
+import { Nav, Card, Button, Container, Row, Col, Form } from 'react-bootstrap';
 import { CustomNavbar } from '../components/CustomNavbar';
 import './MainPage.scss'
-import { useContext } from 'react';
+import { useContext, useMemo, useState } from 'react';
 import { ToastContext } from '../context/ToastProvider';
 import useAuthGuard from '../hooks/useAuthGuard';
 
@@ -17,6 +17,13 @@ export default function MainPage(){
     { id: 5, name: 'Product 2', price: 29.99 },
     { id: 6, name: 'Product 3', price: 39.99 },
   ];
+  const [filters, setFilters] = useState({
+    name: null,
+    price: null
+  })
+
+  const filteredProducts = useMemo(() => products.filter(product => (!filters.name || product.name.toLocaleLowerCase().includes(filters.name.toLocaleLowerCase())) && 
+    (!filters.price || (product.price >= 0 && product.price <= filters.price))), [filters])
 
   const handleAddToCart = (product) => {
     handleSuccessAction('Добавлено в корзину')
@@ -25,13 +32,31 @@ export default function MainPage(){
   return (
     <div className="main page">
         <CustomNavbar/>
-        <Container className='products mt-5'>
+        <Container className='products mt-5 p-0'>
             <Row>
-                <Col md={3} xs={4}></Col>
-                <Col md={9} xs={8}>
+                <Col md={3} xs={4} className="main-filters d-flex flex-column pe-5 ps-0">
+                  <h2>Фильтры:</h2>
+                  <Form className='d-flex flex-column gap-3 mt-4'>
+                    <Form.Group>
+                      <Form.Label>Наименование товара</Form.Label>
+                      <Form.Control
+                        value={filters.name}
+                        onChange={(event) => setFilters({...filters, name: event.target.value})}
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Label>Максимальная цена товара</Form.Label>
+                      <Form.Range
+                        value={filters.price}
+                        onChange={(event) => setFilters({...filters, price: event.target.value})}
+                      />
+                    </Form.Group>
+                  </Form>
+                </Col>
+                <Col md={9} xs={8} className='ps-5'>
                     <h1 className='products-title mb-4'>Каталог товаров</h1>
                     <div className="products-grid">
-                        {products.map((product) => (
+                        {filteredProducts.map((product) => (
                             <Card key={product.id} className='products-card'>
                             <Card.Body>
                                 <Card.Title>{product.name}</Card.Title>
